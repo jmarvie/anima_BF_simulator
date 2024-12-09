99be8f79b6529070536523e0ab9a51a4fbb74189
jomarvie <jonathan.marvie.pro@gmail.com>
2024-12-09 15:59:39 +0100

    first commit

---
+++ src/main/java/com/anima_BF_simulator/AnimaBfSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.anima_BF_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimaBfSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimaBfSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anima_BF_simulator/AnimaBfSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anima_BF_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimaBfSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

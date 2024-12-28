3fdd550e22237091db979a1c00cfc0991f4651ab
jomarvie <jonathan.marvie.pro@gmail.com>
2024-12-28 19:52:06 +0100

    add tests packages and first class

--- src/main/java/com/anima_BF_simulator/utils/TableReader.java
+++ src/main/java/com/anima_BF_simulator/utils/TableReader.java
@@ -1,4 +1,14 @@
 package com.anima_BF_simulator.utils;
 
+import java.io.File;
+import java.util.Objects;
+
 public class TableReader {
+
+    private String readResultsTable (){
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(Objects.requireNonNull(classLoader.getResource("fileTest.txt")).getFile());
+
+        return "";
+    }
 }
---
+++ src/test/java/com/anima_BF_simulator/utils/TableReaderTest.java
@@ -0,0 +1,8 @@
+package com.anima_BF_simulator.utils;
+
+import org.junit.jupiter.api.Test;
+
+public class TableReaderTest {
+
+
+}

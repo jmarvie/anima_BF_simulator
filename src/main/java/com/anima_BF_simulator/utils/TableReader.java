49e1a68c6eb6a024a1f3fac7b46c1475afb68342
jomarvie <jonathan.marvie.pro@gmail.com>
2025-01-28 15:33:05 +0100

    add json methods and tests methods

--- src/main/java/com/anima_BF_simulator/utils/TableReader.java
+++ src/main/java/com/anima_BF_simulator/utils/TableReader.java
@@ -1,15 +1,37 @@
 package com.anima_BF_simulator.utils;
 
+import com.fasterxml.jackson.core.json.UTF8DataInputJsonParser;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Objects;
 
 @Component
 public class TableReader {
 
-    public File readResultsTable (){
+    public File readFileResultsTable (){
         ClassLoader classLoader = getClass().getClassLoader();
-        return new File(Objects.requireNonNull(classLoader.getResource("tableResults.json")).getFile());
+        File resultsFile = new File(Objects.requireNonNull(classLoader.getResource("tableResults.json")).getFile());
+        resultsFile.setWritable(false);
+        return resultsFile;
     }
+
+    public String extractResultsFromFile(File file) {
+        try {
+            return Files.readString(file.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public JSONArray parseStringIntoJSONArray() {
+        String JSONString = extractResultsFromFile(readFileResultsTable());
+        JSONObject obj = new JSONObject(JSONString);
+        return obj.getJSONArray("results");
+    }
+
 }
--- src/test/java/com/anima_BF_simulator/utils/TableReaderTests.java
+++ src/test/java/com/anima_BF_simulator/utils/TableReaderTests.java
@@ -1,11 +1,13 @@
 package com.anima_BF_simulator.utils;
 
+import org.json.JSONArray;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class TableReaderTests {
@@ -13,13 +15,28 @@ public class TableReaderTests {
 @Autowired
 public TableReader reader;
 
-@Test
-void resultsFileExists(){
-    assertTrue(reader.readResultsTable().exists());
-}
+    @Test
+    void resultsFileExists(){
+        assertTrue(reader.readFileResultsTable().exists());
+    }
 
-@Test
-void resultsFileReadable(){
-    assertTrue(reader.readResultsTable().canRead());
-}
+    @Test
+    void resultsFileReadable(){
+        assertTrue(reader.readFileResultsTable().canRead());
+        assertFalse(reader.readFileResultsTable().canWrite());
+    }
+
+    @Test
+    void resultsExists() {
+        File file = reader.readFileResultsTable();
+        assertFalse(reader.extractResultsFromFile(file).isEmpty());
+    }
+
+    @Test
+    void JSONArrayExists() {
+        JSONArray array = reader.parseStringIntoJSONArray();
+        assertFalse(array.isNull(0));
+        assertFalse(array.isNull(481));
+        assertTrue(array.isNull(482));
+    }
 }

79b591c448b36c4f16035896102297a784bac204
jomarvie <jonathan.marvie.pro@gmail.com>
2025-02-05 02:22:42 +0100

    add calculaing methods and unit tests

--- src/main/java/com/anima_BF_simulator/utils/ResultsCalculator.java
+++ src/main/java/com/anima_BF_simulator/utils/ResultsCalculator.java
@@ -1,7 +1,10 @@
 package com.anima_BF_simulator.utils;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class ResultsCalculator {
@@ -9,6 +12,25 @@ public class ResultsCalculator {
     @Autowired
     public TableReader reader;
 
+    public double getResultFromTable(int diceResult, int ip) {
+        JSONArray resultsArray = reader.parseStringIntoJSONArray();
+        double result = 0;
+        for (int i = 0; i < resultsArray.length(); i++) {
+            JSONObject resultArray = resultsArray.getJSONObject(i);
+            if (diceResult >= resultArray.getInt("minLimit") && diceResult <= resultArray.getInt("maxLimit") && ip == resultArray.getInt("ipScore")) {
+                result = resultArray.getDouble("score");
+                break;
+            }
+        }
+        return result;
+    }
 
+    public double getDmgScore(int dmg, int diceResult, int ip) {
+        return ((getResultFromTable(diceResult, ip) / 100) * dmg);
+    }
+
+    public double getCounterAttackScore(int diceResult) {
+        return Math.round(getResultFromTable(diceResult, 0));
+    }
 
 }

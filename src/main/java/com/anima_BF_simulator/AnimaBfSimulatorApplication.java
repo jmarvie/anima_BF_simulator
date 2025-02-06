ee0b922129f7374164f00e9fca22b83a3c6ee28b
jomarvie <jonathan.marvie.pro@gmail.com>
2025-02-06 05:12:40 +0100

    add jasypt and application properties encryption

--- src/main/java/com/anima_BF_simulator/AnimaBfSimulatorApplication.java
+++ src/main/java/com/anima_BF_simulator/AnimaBfSimulatorApplication.java
@@ -1,9 +1,11 @@
 package com.anima_BF_simulator;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class AnimaBfSimulatorApplication {
 
 	public static void main(String[] args) {
